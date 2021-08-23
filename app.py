28ef5009dccc763868d096fd7f4bb4c21322bff2
rajayasaswini <rajayasaswini@gmail.com>
2021-08-23 23:48:15 +0400

    aug 23 - login and signup pages entered

--- app.py
+++ app.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route('/signup', methods=['post', 'get'])
+def signup():
+    return render_template("signup.html")
+
+@app.route('/login', methods=['post', 'get'])
+def login():
+    return render_template("login.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
