090877815af84fe836c115b7cf0f557e445fdf29
rajayasaswini <rajayasaswini@gmail.com>
2021-08-26 19:52:36 +0400

    aug 26 - brushed up on database connections

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, Table
+from sqlalchemy import *
 from sqlalchemy.orm import relationship, backref
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -7,15 +7,15 @@ Base= declarative_base()
 class Users(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True)
-    username = Column(String(100))
-    password = Column(String(255))
-    firstname = Column(String(255))
-    lastname = Column(String(255))
-    email = Column(String(4096))
-    about = Column(String(4096))
-    photo = Column(blob)
+    username = Column(String(100), nullable=False)
+    password = Column(String(255), nullable=False)
+    firstname = Column(String(255), nullable=True)
+    lastname = Column(String(255), nullable=True)
+    email = Column(String(4096),nullable=False)
+    about = Column(String(4096),nullable=True)
+    photo = Column(BLOB)
     classID = Column(String(6), ForeignKey("classes.classID"))
-    isAdmin = Column(Integer(1))
+    isAdmin = Column(Integer, nullable=False)
     userdst = relationship('UserDST', backref='user')
     log = relationship('Logs', backref='user')
     photos = relationship('Photos', backref='user')
@@ -26,7 +26,7 @@ class Classes(Base):
     __tablename__ = "classes"
     classID = Column(Integer, primary_key=True)
     classCode = Column(String(6))
-    classAdminID = Column(Integer)
+    classAdminID = Column(Integer, ForeignKey("users.id"))
     user = relationship('Users', backref='class')
 
 class Events(Base):
@@ -41,32 +41,32 @@ class Events(Base):
 class Logs(Base):
     __tablename__ = "logs"
     userID = Column(Integer, ForeignKey("users.id"))
-    logID = Column(Integer)
-    logContent = Column(string)
-    logDateTime = Column(datetime)
+    logID = Column(Integer, primary_key=True)
+    logContent = Column(String)
+    logDateTime = Column(DateTime)
 
 class Photos(Base):
     __tablename__ = "postphotos"
     userID = Column(Integer, ForeignKey("users.id"))
     postID = Column(Integer, ForeignKey("posts.postID"))
     postPhotoID = Column(Integer, primary_key=True)
-    postPhoto = Column(blob)
-    postDateTime = Column(datetime)
+    postPhoto = Column(BLOB)
+    postDateTime = Column(DateTime)
 
 class Posts(Base):
     __tablename__ = "posts"
     userID = Column(Integer, ForeignKey("users.id"))
     postID = Column(Integer, primary_key=True)
     postContent = Column(String(255))
-    isPosted = Column(Integer(1))
+    isPosted = Column(Boolean)
     photo = relationship("Photos",backref='posts')
 
 class ScheduledAssignments(Base):
     __tablename__ = "scheduledassignments"
-    assignmentID = Column(Integer)
+    assignmentID = Column(Integer, primary_key=True)
     eventID = Column(Integer, ForeignKey("event.eventID"))
-    scheduledDateTime = Column(datetime)
-    returnDateTime = Column(datetime)
+    scheduledDateTime = Column(DateTime)
+    returnDateTime = Column(DateTime)
     userdst = relationship("UserDST", backref='assignment')
 
 class UserDST(Base):
@@ -74,9 +74,9 @@ class UserDST(Base):
     userID = Column(Integer, ForeignKey("users.id"))
     eventID = Column(Integer, ForeignKey("event.eventID"))
     userDSTID = Column(Integer, primary_key=True)
-    datetime = Column(datetime)
+    DateTime = Column(DateTime)
     userDistance = Column(Integer)
-    userTime = Column(time)
+    userTime = Column(Time)
     userSpeed = Column(Integer)
-    isAssignment = Column(tinyint)
+    isAssignment = Column(Integer)
     assignmentID = Column(Integer, ForeignKey("scheduledassignments.assignmentID"), nullable=True)
